2e2a88b9fd78e1ba536ebd6d9fda4357375dcdd5
Greg Johnston <greg.a.johnston@gmail.com>
2016-03-30 14:03:14 -0400

    Changed test file to match master version

--- palindrome_test.py
+++ palindrome_test.py
@@ -1,13 +1,51 @@
-import palindromic
+import unittest
 
-print("\n\nRecursive Testing....\n")
-palindromic.output_result("stunt nuts")
-palindromic.output_result("Lisa Bonet ate no basil.")
-palindromic.output_result("A man, a plan, a cat, a ham, a yak, a hat, a canal: Panama!")
-palindromic.output_result("Doc, note, I dissent. A fast never prevents a fatness. I diet on cod.")
+from palindrome import is_palindrome
+from palindrome import is_palindrome_iterative
 
-print("\n\nIterative Testing....\n")
-palindromic.output_result_iterative("stunt nuts")
-palindromic.output_result_iterative("Lisa Bonet ate no basil.")
-palindromic.output_result_iterative("A man, a plan, a cat, a ham, a yak, a hat, a canal: Panama!")
-palindromic.output_result_iterative("Doc, note, I dissent. A fast never prevents a fatness. I diet on cod.")
+class TestPalindrome(unittest.TestCase):
+    def test_even_numbers(self):
+        self.assertTrue(is_palindrome('toot'))
+
+    def test_odd_numbers(self):
+        self.assertTrue(is_palindrome('tot'))
+
+    def test_simple_values(self):
+        self.assertTrue(is_palindrome('stunt nuts'))
+
+    def test_complete_sentences(self):
+        self.assertTrue(is_palindrome('Lisa Bonet ate no basil.'))
+
+    def test_complex_sentences(self):
+        self.assertTrue(is_palindrome('A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal: Panama!'))
+
+    def test_multiple_sentences(self):
+        self.assertTrue(is_palindrome('Doc, note, I dissent. A fast never prevents a fatness. I diet on cod.'))
+
+    def test_non_palindromes(self):
+        self.assertFalse(is_palindrome('i am not a palindrome'))
+
+    def test_even_numbers_iterative(self):
+        self.assertTrue(is_palindrome_iterative('toot'))
+
+    def test_odd_numbers_iterative(self):
+        self.assertTrue(is_palindrome_iterative('tot'))
+
+    def test_simple_values_iterative(self):
+        self.assertTrue(is_palindrome_iterative('stunt nuts'))
+
+    def test_complete_sentences_iterative(self):
+        self.assertTrue(is_palindrome_iterative('Lisa Bonet ate no basil.'))
+
+    def test_complex_sentences_iterative(self):
+        self.assertTrue(is_palindrome_iterative('A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal: Panama!'))
+
+    def test_multiple_sentences_iterative(self):
+        self.assertTrue(is_palindrome_iterative('Doc, note, I dissent. A fast never prevents a fatness. I diet on cod.'))
+
+    def test_non_palindromes_iterative(self):
+        self.assertFalse(is_palindrome_iterative('i am not a palindrome'))
+
+
+if __name__ == '__main__':
+    unittest.main()
